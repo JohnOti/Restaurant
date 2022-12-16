56d8e9c187b64129e8e25a2295d40d67cf1fa42a
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-12-17 00:23:53 +0300

    add login && register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./About";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
@@ -13,11 +15,24 @@ function App() {
             <li>
               <Link to="/about">About</Link>
             </li>
-            
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
           </ul>
         </nav>
         <Switch>
-          <Route path="/about"><About /></Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
         </Switch>
       </Router>
     </div>
---
+++ client/src/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login'>
+      <form className="forms">
+        <h1 id="user">User Login</h1>
+        <label>Username</label>
+        <input type="text" className="form" placeholder="username" />
+        <label>Email</label>
+        <input type="email" className="form" placeholder="email" />
+        <label>Password</label>
+        <input type="password" className="form" placeholder="Password" />
+        <br />
+        <br />
+        <button className="btn" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login
