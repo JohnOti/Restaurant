68b22e94a1c88b4c82dd3309c1b5c85e27b154f6
George <sbeveme@gmail.com>
2023-01-15 20:22:06 +0300

    update

--- client/src/Components/RestaurantSignUp.js
+++ client/src/Components/RestaurantSignUp.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const RestaurantSignUp = ({ location, onLogin }) => {
     const [error, setErrors] = useState([]);
